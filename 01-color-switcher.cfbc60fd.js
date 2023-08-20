2c3a728b6cb54e05b19409a7f5a05754ff746e49
Soroka0105 <111576195+Soroka0105@users.noreply.github.com>
2023-08-20 14:13:33 +0000

    Deploying to gh-pages from @ Soroka0105/goit-js-hw-09@5fc4c69ab24db4755e58ee71700aca7b68a852e4 🚀

---
+++ 01-color-switcher.cfbc60fd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1;setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.cfbc60fd.js.map
